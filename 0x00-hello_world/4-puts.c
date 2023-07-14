5a657648455f09498c424e748e6100b92ba134ff
atbucks <ahmedtarek1333@gmail.com>
2023-07-14 14:54:44 -0700

    pre

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * this is a program for puts print function and using betty style
+ *
+ * return 0 means success
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
