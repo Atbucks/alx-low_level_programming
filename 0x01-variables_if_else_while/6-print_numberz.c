108be02e8a89123dd17c6a6bb4a0dd86ea4fc49f
atbucks <ahmedtarek1333@gmail.com>
2023-07-16 10:01:07 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+  * main - Print alphabet in lowercase
+  * Description: 'print lower case useing while'
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
