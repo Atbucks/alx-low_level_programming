459a25933f831abe5e5892df1c227333b5cb652c
atbucks <ahmedtarek1333@gmail.com>
2023-07-16 09:14:57 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int n = 97;
 
-	while (n <=122)
+	while (n <= 122)
 	{
 		if (n == 101 || n == 113)
-		{	
+		{
 			n++;
 			continue;
 		}
-	        putchar(n);
-	        n++;
+			putchar(n);
+			n++;
 	}
 	putchar('\n');
 	return (0);
