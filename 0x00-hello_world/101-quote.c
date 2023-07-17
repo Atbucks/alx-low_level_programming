763997f14848f04768515b7f3b330be08bea3b67
atbucks <ahmedtarek1333@gmail.com>
2023-07-17 01:06:09 -0700

    bettyedit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<unistd.h>
 
 /**
- * main - write function to print string by not using puts of printf
- *
- * Return 1 : success
- *
- **/
+  *main - write function to print string by not using puts of printf
+  *Description: printf function
+  *Return: always 0
+  *
+  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
